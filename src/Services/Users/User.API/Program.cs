1674a175b61f5ee2ffab8b0695c6a0f7f8f5eeee
Sergej Steinsiek <sergejsteinsiek@proton.me>
2024-12-18 10:50:17 +0100

    Add Serilog

--- src/Services/Users/User.API/Program.cs
+++ src/Services/Users/User.API/Program.cs
@@ -1,4 +1,11 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .CreateLogger();
+builder.Host.UseSerilog();
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -6,6 +13,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseSerilogRequestLogging();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
